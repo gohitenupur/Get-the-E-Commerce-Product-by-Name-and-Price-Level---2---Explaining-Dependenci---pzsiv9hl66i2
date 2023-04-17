c50bc8e0eddd71873519fb2411a7c4d51e29def1
Nupur Gohite <118847362+gohitenupur@users.noreply.github.com>
2023-04-17 22:48:16 +0530

    upd

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ const app = express();
 const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
 
 //Middlewares
+app.get("/api/v1/products/:id", (req, res) => {
+  const ix = req.params.id;
+  console.log(ix);
+  let val = products.find((item) => {
+    return item.id == ix;
+  });
+
+  if (val) {
+    console.log(val);
+    res.status(200).json({
+      status: "success",
+      message: "Product fetched successfully",
+      data: {
+        product: val
+      }
+    });
+  } else {
+    res.status(404).json({ status: "failed", message: "Product not found!" });
+  }
+});
+
 app.use(express.json());
 
 // GET endpoint for sending the products to client by id
